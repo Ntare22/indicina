fb7c25a6134f4c05cecfd99daefcfc9c2dfbe3fe
Ntare22 <jim.ntare@gmail.com>
2025-05-03 22:24:23 +0300

    fix test files

--- src/url-encoder/url-encoder.controller.spec.ts
+++ src/url-encoder/url-encoder.controller.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UrlEncoderController } from './url-encoder.controller';
+import { UrlEncoderService } from './url-encoder.service';
 
 describe('UrlEncoderController', () => {
   let controller: UrlEncoderController;
+  let service: UrlEncoderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UrlEncoderController],
+      providers: [UrlEncoderService],
     }).compile();
 
     controller = module.get<UrlEncoderController>(UrlEncoderController);
+    service = module.get<UrlEncoderService>(UrlEncoderService);
   });
 
   it('should be defined', () => {
