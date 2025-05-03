d2e020886fb3e12f37b4c7af8a6d5acc6e2ff456
Ntare22 <jim.ntare@gmail.com>
2025-05-03 18:54:23 +0300

    setup github workflows & continous integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UrlEncoderModule } from './url-encoder/url-encoder.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UrlEncoderModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/url-encoder/url-encoder.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('url-encoder')
+export class UrlEncoderController {}
